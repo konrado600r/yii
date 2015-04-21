7c1ca28281f002a48bd54b872f9db61d80bb6757
kmk <kmk@w500marta>
2015-04-21 14:06:59 +0200

    test

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=16548328_yii',
+    'username' => '16548328_yii',
+    'password' => 'xmry8%Dx',
+    'charset' => 'utf8',
+];
